321f2c918756c9c4297e6ba8ddee96aece158e31
Reed Martin <reedhmartin@gmail.com>
2023-04-11 18:41:12 -0400

    Added todos in connection.js

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+// TODO: Import required packages
+// - Import MySQL2 package
+
+// TODO: Set up MySQL connection
+// - Create object with these properties:
+//   * host
+//   * port
+//   * user
+//   * password
+//   * database
+
+// TODO: Create MySQL connection using the config object
+
+// TODO: Connect to MySQL server and SQL database
+
+// TODO: Export connection object
