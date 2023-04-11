3bc208033488f87085e82b932e8f42aca99b1e6b
Reed Martin <reedhmartin@gmail.com>
2023-04-11 19:02:55 -0400

    Added todos to seeds file. 3 depts, 5 roles, and 8 emps for testing.

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+-- TODO: Insert sample data into the `department` table
+-- - Insert 3 different departments.
+
+-- TODO: Insert sample data into the `role` table
+-- - Insert 5 different roles with unique titles, salaries, and corresponding department_ids.
+-- - Make sure the department_ids match the departments inserted earlier.
+
+-- TODO: Insert sample data into the `employee` table
+-- - Insert 8 different employees with unique first_name and last_name combinations, role_ids, and manager_ids.
+-- - Make sure the role_ids match the roles inserted earlier.
+-- - Set the manager_id for each employee to reference other employee ids or set it to NULL for employees without a manager.
