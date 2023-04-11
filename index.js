3e160de72ea111b354cbe9f8040de9cb0ea7e04f
Reed Martin <reedhmartin@gmail.com>
2023-04-11 18:44:42 -0400

    Added todos in index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+// TODO: Import required packages
+// - Import inquirer
+// - Import console.table
+// - Import the Department, Employee, and Role classes
+
+// TODO: Set up inquirer prompt configurations
+
+// TODO: Create the function to initialize the application
+
+// TODO: Create functions for each action
+// - View all departments, roles, and employees.
+// - Add a department, role, and employee.
+// - Update an employee's role.
+
+// TODO: Call the initialize function to start the application
