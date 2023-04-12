5a42987dc41f7597424b5ae5b4dafa624d09de41
Reed Martin <reedhmartin@gmail.com>
2023-04-12 18:28:10 -0400

    Completed connection.js file.

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,23 @@
-// TODO: Import required packages
-// - Import MySQL2 package
+// Import MySQL2 package
+const mysql = require("mysql2");
 
-// TODO: Set up MySQL connection
-// - Create object with these properties:
-//   * host
-//   * port
-//   * user
-//   * password
-//   * database
+// Set up MySQL connection
+const connectionConfig = {
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "employee_tracker_db",
+  };
 
-// TODO: Create MySQL connection using the config object
+// Create MySQL connection using the config object
+const connection = mysql.createConnection(connectionConfig);
 
-// TODO: Connect to MySQL server and SQL database
-
-// TODO: Export connection object
+// Connect to MySQL server and SQL database
+connection.connect((err) => {
+    if (err) throw err;
+    console.log("Connected to the database.");
+  });
+  
+  // Export connection object
+  module.exports = connection;
