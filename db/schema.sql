e1e29797bf78c8e18332a681e57b5e85544e3696
Reed Martin <reedhmartin@gmail.com>
2023-04-12 18:48:00 -0400

    added schema for emp table.

--- db/schema.sql
+++ db/schema.sql
@@ -13,9 +13,13 @@ CREATE TABLE role (
   FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
--- TODO: Create the schema for the `employee` table
--- - id: INT PRIMARY KEY
--- - first_name: VARCHAR(30) to hold employee first name
--- - last_name: VARCHAR(30) to hold employee last name
--- - role_id: INT to hold reference to employee role
--- - manager_id: INT to hold reference to another employee that is the manager of the current employee (null if the employee has no manager)
+-- Create the schema for the `employee` table
+CREATE TABLE employee (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
